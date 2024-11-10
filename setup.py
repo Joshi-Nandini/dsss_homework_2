3b7be3eb0ef8df5056b975631c82615232b3d244
Joshi-Nandini <nandini.joshi@fau.de>
2024-11-10 22:00:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,18 +13,17 @@ setup(
     url="https://github.com/Joshi-Nandini/dsss_homework_2",
     license="Apache License 2.0",
     install_requires=[
-        "required_package1>=1.0.0",         # List your dependencies here
-        "required_package2>=2.0.0"
+        "required_package1>=1.0.0",   
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',                # Minimum Python version required
+    python_requires='>=3.6',               
     entry_points={
         "console_scripts": [
-            "your_command=your_module:main_function",  # Command-line interface entry point
+            "your_command=your_module:main_function",  
         ],
     },
 )
