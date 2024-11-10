923abee4fc42d31e12f52fd0b45b4a0c88866a60
Joshi-Nandini <nandini.joshi@fau.de>
2024-11-10 20:35:24 +0100

    Update math_quiz.py

--- math_quiz.py
+++ math_quiz.py
@@ -1,46 +1,73 @@
 import random
 
-
-def function_A(min, max):
+def integergeneration(min, max):
     """
-    Random integer.
+    Generating random integer value between given min and max value
+
+    Patameters: min = minimum integer value
+                max = maximum integer value
+
+    Return:     Random integer value between given min and max value
     """
     return random.randint(min, max)
 
 
-def function_B():
+def opterationgeneration():
+    """
+    Generate random operator among the following given operators
+
+    Return:     Random operator among the following given operator
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def questiongeneration(int1, int2, opt):
+    """
+    Generate mathematical question based on the operator given and calculate the result with given ineteger values
+
+    Paratemers: int1 = first integer
+                int2 = second integer
+                opt  = operator
+
+    Return:     A tuple containing the question and it's result
+    """
+    question = f"{int1} {opt} {int2}"
+    if opt == '+': 
+        result = int1 - int2
+    elif opt == '-': 
+        result = int1 + int2
+    else: 
+        result = int1 * int2
+    return question, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    no_questions = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(no_questions):
+        #Generate random integers between given min and max values and generate a random operator
+        int1 = integergeneration(1, 10); 
+        int2 = integergeneration(1, 55); 
+        opt = opterationgeneration()
+        #generate the question
+        QUESTION, RESULT = questiongeneration(int1, int2, opt)
+        print(f"\nQuestion: {QUESTION}")
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
+        #Get answer from user
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
+        #Check the user answer with result
+        if useranswer == RESULT:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {RESULT}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{no_questions}")
 
 if __name__ == "__main__":
     math_quiz()
